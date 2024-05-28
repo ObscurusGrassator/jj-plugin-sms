eae17dec701a4fe46a7be62076544e9a934d11b9
ObscurusGrassator <obscurus.grassator@gmail.com>
2024-05-28 12:22:26 +0200

    v2.0.2

--- buildReactNative/interfaceForAI.js
+++ buildReactNative/interfaceForAI.js
@@ -1,1 +1,30 @@
-var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");var _asyncToGenerator2=_interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));var _classCallCheck2=_interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));var _createClass2=_interopRequireDefault(require("@babel/runtime/helpers/createClass"));module.exports=function(){function _class(){(0,_classCallCheck2.default)(this,_class);}(0,_createClass2.default)(_class,[{key:"getContactByName",value:(function(){var _getContactByName=(0,_asyncToGenerator2.default)(function*(name){return null;});function getContactByName(_x){return _getContactByName.apply(this,arguments);}return getContactByName;}())},{key:"sendMessage",value:(function(){var _sendMessage=(0,_asyncToGenerator2.default)(function*(smsNumber,message,fullName){});function sendMessage(_x2,_x3,_x4){return _sendMessage.apply(this,arguments);}return sendMessage;}())},{key:"getMessages",value:(function(){var _getMessages=(0,_asyncToGenerator2.default)(function*(){var options=arguments.length>0&&arguments[0]!==undefined?arguments[0]:{};return null;});function getMessages(){return _getMessages.apply(this,arguments);}return getMessages;}())}]);return _class;}();
+module.exports = class {
+
+    /**
+     * @param { string } name
+     * @returns { Promise<{number: string, fullName: string}> }
+     */
+    async getContactByName(name) { return null; }
+
+    /**
+     * @param { string } smsNumber
+     * @param { string } message
+     * @param { string } [fullName]
+     * @returns { Promise<void> }
+     */
+    async sendMessage(smsNumber, message, fullName) {}
+
+    /**
+     * Returns not readed messages array by sender mame from SMS
+     * @param { Object } [options]
+     * @param { boolean } [options.makrAsReaded = true]
+     * @param { 'inbox' | 'sent' | 'draft' | 'outbox' | 'failed' | 'queued' | 'all' } [options.box = 'inbox']
+     * @param { number } [options.minDateMilliseconds]
+     * @param { number } [options.maxDateMilliseconds]
+     * @param { string } [options.findMessageByRegex]
+     * @param { boolean } [options.onlyRead = false]
+     * @param { string } [options.smsFromNumber]
+     * @returns { Promise<{[number: string]: {messages: string[], timestamp: number, number: string, fullName: string}}> }
+     */
+    async getMessages(options = {}) { return null; }
+};
